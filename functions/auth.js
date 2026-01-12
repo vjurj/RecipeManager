11bc2a841dd4dd9dea75f7a832ffe7357f1b7a50
Jurj Vlad <jurjvlad96@gmail.com>
2026-01-12 20:45:55 +0200

    Added cloudflare function

---
+++ functions/auth.js
@@ -0,0 +1,31 @@
+export async function onRequest(context) {
+  const url = new URL(context.request.url);
+  const code = url.searchParams.get("code");
+
+  if (!code) {
+    return new Response("Missing code", { status: 400 });
+  }
+
+  const clientId = context.env.GITHUB_CLIENT_ID;
+  const clientSecret = context.env.GITHUB_CLIENT_SECRET;
+
+  const tokenRes = await fetch(
+    `https://github.com/login/oauth/access_token`,
+    {
+      method: "POST",
+      headers: { "Content-Type": "application/json", "Accept": "application/json" },
+      body: JSON.stringify({
+        client_id: clientId,
+        client_secret: clientSecret,
+        code
+      })
+    }
+  );
+
+  const tokenData = await tokenRes.json();
+
+  return Response.redirect(
+    `/admin/#access_token=${tokenData.access_token}`,
+    302
+  );
+}
