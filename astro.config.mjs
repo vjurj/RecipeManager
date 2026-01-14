4e518681147a8a4d400550d11eeb481ceca369e4
Jurj Vlad <jurjvlad96@gmail.com>
2026-01-14 19:34:56 +0200

    More changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://recipemanager.pages.dev',
+    site: 'https://vjurj.github.io',
+    base: '/RecipeManager'
 })
